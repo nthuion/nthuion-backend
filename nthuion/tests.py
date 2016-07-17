cb380c84c73e7241a3217e2e12b91112b7422ebe
afg <afg984@gmail.com>
2016-07-17 20:50:29 +0800

    test also invalid request

--- nthuion/tests.py
+++ nthuion/tests.py
@@ -88,6 +88,8 @@ class TestEcho(WebTest):
             '/api/echo',
             params=self.payload,
             status=200)
+
+        # query string does not have
         self.assertEqual(
             {'nthu': ['ion'], 'afg': ['984']}, res.json)
 
@@ -97,6 +99,18 @@ class TestEcho(WebTest):
             params=self.payload,
             status=200)
         self.assertEqual(self.payload, res.json)
+
+    def test_invalid_request(self):
+        res = self.app.post(
+            '/api/echo',
+            params=self.payload,
+            status=400
+        )
+        res = self.app.put(
+            '/api/echo',
+            params=self.payload,
+            status=400
+        )
 
     def test_put(self):
         res = self.app.put_json(
